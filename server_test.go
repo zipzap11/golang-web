c6c548496098819426e4931802dfdada297c3001
Francisco <fcisco.dev@gmail.com>
2021-12-18 15:11:04 +0700

    add query case

--- server_test.go
+++ server_test.go
@@ -18,8 +18,10 @@ func TestServer(t *testing.T) {
 		fmt.Fprintln(writer, request.RequestURI)
 		fmt.Fprintln(writer, request.Body)
 	})
-	router.HandleFunc("/login", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprint(writer, "login page")
+	router.HandleFunc("/login/", func(writer http.ResponseWriter, request *http.Request) {
+		param := request.URL.Query()
+		name := param.Get("name")
+		fmt.Fprintf(writer, "login page %v", name)
 	})
 	router.HandleFunc("/register", func(writer http.ResponseWriter, request *http.Request) {
 		fmt.Fprint(writer, "register page")
