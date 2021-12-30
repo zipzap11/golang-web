af77db880009ed7ab1115bbd682d4abfcae83f74
Francisco <fcisco.dev@gmail.com>
2021-12-30 16:38:59 +0700

    request mock for unit test

---
+++ http_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func HelloHandler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintln(writer, "hello")
+}
+
+func TestHttp(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello", nil)
+	recorder := httptest.NewRecorder()
+
+	HelloHandler(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	bodyString := string(body)
+
+	fmt.Println(bodyString)
+}
