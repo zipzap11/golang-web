fea58e0e2ce0a04d62e1a01e0b030eb5420f0d6a
Francisco <fcisco.dev@gmail.com>
2021-12-31 14:39:59 +0700

    add redirect usecase

---
+++ redirect_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func RedirectTo(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprint(writer, "Redirected")
+}
+
+func RedirectFrom(writer http.ResponseWriter, request *http.Request) {
+	http.Redirect(writer, request, "/redirect-to", http.StatusTemporaryRedirect)
+}
+
+func TestServerRedirect(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/redirect-from", RedirectFrom)
+	mux.HandleFunc("/redirect-to", RedirectTo)
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	server.ListenAndServe()
+}
